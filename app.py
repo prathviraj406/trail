7d0cdc7f225f9a6530909a1176a17711f5e137d9
prathviraj406 <98145915+prathviraj406@users.noreply.github.com>
2024-08-06 17:17:22 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
+def start():
+    return "The MBSA Server is Running"
+
+@app.route("/a")
 def index():
     return render_template("index.html")
 
